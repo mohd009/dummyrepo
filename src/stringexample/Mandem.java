90b9b102c0fceb588ea5aa03ef933f9f7b8f1aae
mohd009 <abdulnmo@sheridancollege.ca>
2020-02-14 19:56:57 -0500

    New feature added

---
+++ src/stringexample/Mandem.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stringexample;
+
+/**
+ *
+ * @author User
+ */
+public class Mandem {
+    public Mandem(){
+        
+    }
+    public void Test(){
+        
+    }
+}
